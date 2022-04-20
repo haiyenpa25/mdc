5423a26766394066e1af3c5e5c471209b7ab8e28
HaiNguyen CNS <hainguyen.iti@gmail.com>
2022-04-20 16:56:57 +0700

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,15 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    @yield('layouts.head')    
+  @extends('layouts.head')   
 </head>
 <body class="hold-transition sidebar-mini layout-fixed">
 <div class="wrapper">
 
   <!-- Preloader -->
   <div class="preloader flex-column justify-content-center align-items-center">
-    <img class="animation__shake" src="dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
+    <img class="animation__shake" src="{{ asset('admin/dist/img/AdminLTELogo.png'); }}" alt="AdminLTELogo" height="60" width="60">
   </div>
 
   <!-- Navbar -->
@@ -1429,6 +1430,6 @@
     <!-- /.content -->
   </div>
   <!-- /.content-wrapper -->
-  @yield('layouts.footer') 
+  @extends('layouts.footer')) 
 </body>
 </html>
